4525818235b6dba0303b9700d87f6cb82b99ada4
Seung Hun Lee <seunghunkimchi@korea.ac.kr>
2024-07-18 01:36:44 +0900

    Update palindrome-number.java

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0){
+        if(x < 0){ //checks if it's negative
             return false;
         }
-        long reversed = 0;
-        long temp = x;
+        long reversed = 0; // initial reverse
+        long temp = x; // temporary variable
 
-        while (temp != 0){
-            int number = (int) (temp%10);
-            reversed = reversed * 10 + number;
-            temp /= 10;
+        while (temp != 0){ // while temp doesn't equal 0
+            int number = (int) (temp%10); // remainder aka last digit
+            reversed = reversed * 10 + number; // push in the remainder
+            temp /= 10; // pop out the last digit
         }
-        return(reversed == x);
+        return(reversed == x); // check if reversed is equal to x
     }
 }
