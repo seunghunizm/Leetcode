5525a706cb969edeecf60aafaf6e9eb5fc2ea3d8
Seung Hun Lee <seunghunkimchi@korea.ac.kr>
2024-07-18 01:45:43 +0900

    Update two-sum.java
    Adding Comments for fun

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> numMap = new HashMap<>();
-        int n = nums.length;
+        Map<Integer, Integer> numMap = new HashMap<>(); // create a numMap hash map
+        int n = nums.length; // n is the length of nums
 
-        for (int i = 0; i < n; i++) {
-            numMap.put(nums[i], i);
+        for (int i = 0; i < n; i++) { // for loop for the lenth of the n(length of array)
+            numMap.put(nums[i], i); // build/input the hash map
         }
 
-        for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (numMap.containsKey(complement) && numMap.get(complement) != i) {
-                return new int[]{i, numMap.get(complement)};
+        for (int i = 0; i < n; i++) { //for loop to compare
+            int complement = target - nums[i]; // complement is the target minux the current number
+            if (numMap.containsKey(complement) && numMap.get(complement) != i) { // check if the map contains that complement and the get complement isn't pointing to the current numbe
+                return new int[]{i, numMap.get(complement)}; // return that current integer and that complement's index 
             }
         }
 
