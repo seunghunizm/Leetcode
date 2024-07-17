7a1d13648b253bd1a0b3b108d2ff83445e1c0085
Seung Hun Lee <seunghunkimchi@korea.ac.kr>
2024-07-18 01:58:36 +0900

    Time: 9 ms (6.68%) | Memory: 44.6 MB (41.72%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> m = new HashMap<>(); // create a hashmap to input the roman numerals with its respective value
+        m.put('I', 1); // I -> 1
+        m.put('V', 5); // V -> 5
+        m.put('X', 10); // X -> 10
+        m.put('L', 50); // L -> 50
+        m.put('C', 100); // C -> 100
+        m.put('D', 500); // D -> 500
+        m.put('M', 1000); // M -> 1000
+
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++){ // for loop for the length of string S
+            if (i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
+                ans -= m.get(s.charAt(i));
+            } else {
+                ans += m.get(s.charAt(i));
+            }
+        }
+        return ans;
+    }
+}
