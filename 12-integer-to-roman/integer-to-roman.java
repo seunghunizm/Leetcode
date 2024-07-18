7766c5661227a3c5c23bf055c4f2b066f98bfe1c
Seung Hun Lee <seunghunkimchi@korea.ac.kr>
2024-07-18 17:02:27 +0900

    Time: 3 ms (97.70%) | Memory: 44.1 MB (70.52%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,9 +1,24 @@
 class Solution {
     public String intToRoman(int num) {
-        String thousands[] = {"", "M", "MM", "MMM"};
-        String hundreds[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-        String tens[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-        String ones[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-        return thousands[num/1000] + hundreds[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+        
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < values.length && num > 0; i++) {
+            
+            while (num >= values[i]) {
+                num -= values[i];
+                result.append(symbols[i]);
+            }
+        }
+        
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.intToRoman(3749)); 
     }
 }
