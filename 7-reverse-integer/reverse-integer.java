13ac918632feadf9fbffa4b2cc218cdb9a297505
Seung Hun Lee <seunghunkimchi@korea.ac.kr>
2024-07-21 21:39:52 +0900

    Time: 1 ms (76.31%) | Memory: 40.8 MB (33.44%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -2,17 +2,19 @@ class Solution {
     public int reverse(int x) {
         int reverse = 0;
         int temp;
+        int maxDiv10 = Integer.MAX_VALUE / 10;
+        int minDiv10 = Integer.MIN_VALUE / 10;
 
         while (x != 0) {
-          temp = x % 10;
-          if (reverse > Integer.MAX_VALUE / 10 || (reverse == Integer.MAX_VALUE / 10 && temp > 7) ||
-              reverse < Integer.MIN_VALUE / 10 || (reverse == Integer.MIN_VALUE / 10 && temp < -8)) {
-           return 0;
-         }
-         reverse = reverse * 10 + temp;
-         x /= 10;
+            temp = x % 10;
+            if (reverse > maxDiv10 || (reverse == maxDiv10 && temp > 7) ||
+                reverse < minDiv10 || (reverse == minDiv10 && temp < -8)) {
+                return 0;
+            }
+            reverse = reverse * 10 + temp;
+            x /= 10;
         }
 
-        return (int)reverse;
+        return reverse;
     }
 }
