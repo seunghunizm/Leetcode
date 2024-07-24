54e39363003c99ac843fb088a8e5b5a5845dc4eb
Seung Hun Lee <seunghunkimchi@korea.ac.kr>
2024-07-25 01:52:05 +0900

    Update group-anagrams.java
    comments

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,18 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
-        if (strs == null || strs.length == 0){
-            return new ArrayList<>();
+        if (strs == null || strs.length == 0){ // if nothing just return it
+            return new ArrayList<>(); // could shorten by using map.putIfAbsent(sortedWord, new ArrayList<>());
         }
         for (String anagram : strs) {
             char[] characters = anagram.toCharArray();
-            Arrays.sort(characters);
+            Arrays.sort(characters); // we get each word sort the word then check it with the originally sorted word to see if it matches
             String key = String.valueOf(characters);
 
             if (!map.containsKey(key)) {
-                map.put(key, new ArrayList<>());
+                map.put(key, new ArrayList<>()); // if it doesn't contain the key aka the original sorted word make new
             }
-            map.get(key).add(anagram);
+            map.get(key).add(anagram); // get the key and then add it
         }
         return new ArrayList<>(map.values());
     }
